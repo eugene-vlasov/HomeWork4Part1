7bf33530b38153bb74fa95dc3e399f63055e42d3
eugene-vlasov <eugene.vlasov17@gmail.com>
2024-12-03 05:23:27 +0200

    Added empty project

---
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork4Part1
+//
+//  Created by Evgene Vlasov on 03.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
